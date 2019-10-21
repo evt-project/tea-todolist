8b5f472d75d18de3a91046c71f0f10331c1ee413
federicaspinelli <federicaspinelli@msn.com>
2019-10-21 22:23:36 +0200

    Add new todo list component

--- src/app/add-new-todo-list/add-new-todo-list.component.ts
+++ src/app/add-new-todo-list/add-new-todo-list.component.ts
@@ -1,25 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-new-todo-list',
   templateUrl: './add-new-todo-list.component.html',
   styleUrls: ['./add-new-todo-list.component.scss']
 })
-export class AddNewTodoListComponent {
-  title = 'Add-new-todo-list';
-  data: AppData = {todos: [
-    {title: 'First todo', completed: true},
-    {title: 'Second todo', completed: false}]};
-}
+export class AddNewTodoListComponent implements OnInit {
 
-interface TodoItem {
-  list: string;
-  id: number;
-  title: string;
-  description?: string;
-  completed: boolean;
-}
+  constructor() { }
 
-export interface AppData {
-  todos: TodoItem[];
+  ngOnInit() {
+  }
+
 }
