a1f94b05f57bb6c29e91f1265a275413b90a16c9
federicaspinelli <federicaspinelli@msn.com>
2019-10-22 15:08:58 +0200

    Remove empty lifecycle hook

--- src/app/add-new-todo-list/add-new-todo-list.component.ts
+++ src/app/add-new-todo-list/add-new-todo-list.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-new-todo-list',
   templateUrl: './add-new-todo-list.component.html',
   styleUrls: ['./add-new-todo-list.component.scss']
 })
-export class AddNewTodoListComponent implements OnInit {
-  ngOnInit() {
-  }
-
+export class AddNewTodoListComponent {
 }
