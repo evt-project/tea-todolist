833a729838cfff90af8968299e31dc3e139cf42b
Giacomo Cerrretini <cerre.giacomo93@gmail.com>
2019-10-23 16:23:09 +0200

    Add ngModel for the description

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit} from '@angular/core';
 
 export class AddItemComponent implements OnInit {
   itemTitle: string;
-  itemDescription?: string;
+  itemDescription: string;
 
   ngOnInit() {
     this.itemTitle = '';
