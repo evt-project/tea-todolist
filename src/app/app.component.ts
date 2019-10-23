35fe62b73530219517683dc9c5a13c78f113a4bd
federicaspinelli <federicaspinelli@msn.com>
2019-10-23 16:38:24 +0200

    Resolved conflicts manually after rebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Add-new-todo-list';
-  data: AppData = {todos: [{
-    title: 'First todo', completed: true},
-    {title: 'Second todo', completed: false}]};
-}
-export interface Todo {
-  title: string;
-  completed: boolean;
-}
-export interface AppData {
-  todos: Array<Todo>;
+  title = 'tea-todolist';
 }
