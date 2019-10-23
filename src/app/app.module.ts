58d98850fabe08bae38f39bed3aabb2dcb59fd92
jurgen memaj <MJ.std3@gmail.com>
2019-10-23 16:23:26 +0200

    visualise a todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+=======
+>>>>>>> visualise a todo list
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AddItemComponent } from './add-item/add-item.component';
 import { AppComponent } from './app.component';
-import { ViewItemComponent } from './view-item/view-item.component';
+import { ItemsComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     AddItemComponent,
     ViewItemComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+=======
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+>>>>>>> visualise a todo list
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
