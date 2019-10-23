489409c331b0cfc69ecd16b241b61291960e1533
Giacomo Cerrretini <cerre.giacomo93@gmail.com>
2019-10-23 16:17:57 +0200

    Add FormModules and AddItemComponent to the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AddItemComponent } from './add-item/add-item.component';
 import { AppComponent } from './app.component';
 import { ViewItemComponent } from './view-item/view-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AddItemComponent,
     ViewItemComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
