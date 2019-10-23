122bdbb21f3f29a60976ffa5f5418cf24bf6d1bc
Giacomo Cerrretini <cerre.giacomo93@gmail.com>
2019-10-23 16:17:06 +0200

    Add add-item component

---
+++ src/app/add-item/add-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-add-item',
+  templateUrl: './add-item.component.html',
+  styleUrls: ['./add-item.component.scss'],
+})
+
+export class AddItemComponent implements OnInit {
+  itemTitle: string;
+  itemDescription?: string;
+
+  ngOnInit() {
+    this.itemTitle = '';
+    this.itemDescription = '';
+  }
+
+  addItem() {
+    console.log(this.itemTitle);
+    console.log(this.itemDescription);
+    this.ngOnInit();
+  }
+
+}
