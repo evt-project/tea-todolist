7faf5f71b679e8893d0a96696dd1ce25db7f5c1f
federicaspinelli <federicaspinelli@msn.com>
2019-10-23 16:38:24 +0200

    Resolve conflicts manually after rebase

--- src/app/add-new-todo-list/add-new-todo-list.component.ts
+++ src/app/add-new-todo-list/add-new-todo-list.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-todo-list.component.scss']
 })
 export class AddNewTodoListComponent {
+  title = 'Add-new-todo-list';
+  data: AppData = {todos: [{
+    title: 'First todo', completed: true},
+    {title: 'Second todo', completed: false}]};
+}
+export interface Todo {
+  title: string;
+  completed: boolean;
+}
+export interface AppData {
+  todos: Array<Todo>;
 }
