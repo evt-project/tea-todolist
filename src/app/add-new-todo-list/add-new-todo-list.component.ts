99f4685ec41968d7fbec3142c6ec505366223e66
federicaspinelli <federicaspinelli@msn.com>
2019-10-28 20:11:33 +0100

    Reorder alphabetically

--- src/app/add-new-todo-list/add-new-todo-list.component.ts
+++ src/app/add-new-todo-list/add-new-todo-list.component.ts
@@ -6,20 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-new-todo-list.component.scss']
 })
 export class AddNewTodoListComponent {
-  title = 'Add-new-todo-list';
-  data: AppData = {todos: [
-    {title: 'First todo', completed: true},
-    {title: 'Second todo', completed: false}]};
-}
-
-interface TodoItem {
-  list: string;
-  id: number;
-  title: string;
-  description?: string;
-  completed: boolean;
-}
-
-export interface AppData {
-  todos: TodoItem[];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { FormsModule } from '@angular/forms';
-import { AddItemComponent } from './add-item/add-item.component';
-import { AppComponent } from './app.component';
-import { AddNewTodoListComponent } from './add-new-todo-list/add-new-todo-list.component';
-import { ViewItemComponent } from './view-item/view-item.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AddItemComponent } from './add-item/add-item.component';
+import { AddNewTodoListComponent } from './add-new-todo-list/add-new-todo-list.component';
+import { AppComponent } from './app.component';
 import { ViewItemComponent } from './view-item/view-item.component';
 
 @NgModule({
