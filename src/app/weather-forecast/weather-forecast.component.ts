09b3f99a1a323798ecf490bcc17e707fba485f2e
Memmo91 <dalischi@hotmail.it>
2019-11-02 08:40:42 +0100

    Add weather forecast feature to hmtl page

---
+++ src/app/weather-forecast/weather-forecast.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import { WeatherForecastApiService } from './weatherForecastApiService/weather-forecast-api.service';
+
+@Component({
+    selector: 'app-weather-forecast',
+    templateUrl: './weather-forecast.component.html',
+    styleUrls: ['./weather-forecast.component.scss']
+})
+export class WeatherForecastComponent implements OnInit {
+    response: object;
+    constructor(private wfas: WeatherForecastApiService) {}
+
+    ngOnInit() {
+        this.retriveWF();
+        console.log('non so se funziona');
+    }
+    private retriveWF() {
+        this.wfas.getOBS().subscribe((response) => {
+            this.response = response;
+            // console.log(this.response);
+        });
+    }
+}
