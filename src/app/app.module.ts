8ad385e3e6a0cff7952bcb1dec2cf851e2b0a3f4
Memmo91 <dalischi@hotmail.it>
2019-11-02 18:51:31 +0100

    Update weather forecast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,6 +7,7 @@ import { AddItemComponent } from './add-item/add-item.component';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { ViewItemComponent } from './view-item/view-item.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { ViewItemComponent } from './view-item/view-item.component';
     AppComponent,
     ListComponent,
     ViewItemComponent,
+    WeatherForecastComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
