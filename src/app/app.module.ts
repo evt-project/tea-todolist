9bcee022cac8ba273cd956d5ae519ae045966fb1
jurgen memaj <MJ.std3@gmail.com>
2019-11-03 21:27:45 +0100

    Define item interface through service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AddItemComponent } from './add-item/add-item.component';
 import { AppComponent } from './app.component';
 import { ListItemComponent } from './list-item/list-item.component';
-import { ViewItemComponent } from './view-item/view-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddItemComponent,
     ListItemComponent,
-    ViewItemComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,34 +1,15 @@
 import { Component } from '@angular/core';
-import { TodoItem } from '../view-item/view-item.component';
+import { DataService } from '../service/data-service.service';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
+  providers: [DataService],
 })
 export class ListItemComponent {
-
-  items: TodoItem [] = [
-    {
-      list: 'First',
-      id: 1,
-      title: 'First title',
-      description: 'an example list item for the view model',
-      completed: false,
-    },
-    {
-      list: 'Second',
-      id: 1,
-      title: 'Second title',
-      description: 'an example list item for the view model',
-      completed: false,
-    },
-    {
-    list: 'Third',
-    id: 1,
-    title: 'Third title',
-    description: 'an example list item for the view model',
-    completed: false,
-    },
-  ];
+  itemsList = [];
+  constructor(dataService: DataService) {
+    this.itemsList = dataService.listData();
+  }
 }
