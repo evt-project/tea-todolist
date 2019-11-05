b23bdfdcd7b73ee9a2cd2f1db2a1945ce7929855
federicaspinelli <federicaspinelli@msn.com>
2019-11-05 15:59:18 +0100

    Make an object with the type of a list

--- src/app/add-new-todo-list/add-new-todo-list.component.ts
+++ src/app/add-new-todo-list/add-new-todo-list.component.ts
@@ -1,23 +1,21 @@
 import { Component } from '@angular/core';
+import { DataService } from '../service/data-service.service';
+import { List } from '../model';
 
 @Component({
   selector: 'app-add-new-todo-list',
   templateUrl: './add-new-todo-list.component.html',
   styleUrls: ['./add-new-todo-list.component.scss'],
+  providers: [DataService],
 })
 export class AddNewTodoListComponent {
-  List = [];
-  id = '';
-  listTitle = '';
-  item = [];
-
-  addList() {
-    this.List.push({
-      list: this.item,
-      // add id
-      name: this.listTitle,
-      item: this.item,
-    });
-    this.listTitle = '';
+  list: List = {
+    id = '',
+    name = '',
+    item = [],
+  };
+  constructor(dataService: DataService) {
+    {
+    this.list = dataService.listData();
   }
 }
